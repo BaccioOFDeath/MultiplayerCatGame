17c186238adfdc089cd25d7da6a478d5a0f96532
garettdaly <garett@sdeuropean.co.nz>
2024-10-20 20:53:44 +1300

    Initial Commit

---
+++ MultiplayerCatGameTests/MultiplayerCatGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplayerCatGameTests.swift
+//  MultiplayerCatGameTests
+//
+//  Created by Garett Daly on 20/10/2024.
+//
+
+import Testing
+@testable import MultiplayerCatGame
+
+struct MultiplayerCatGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
